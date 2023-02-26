69d05e2cea973d702bc5516f2378101b267662bd
Derbson <pedrin.olinda@gmail.com>
2023-02-26 18:26:30 -0300

    criação do models perfil

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Perfil
 
-# Register your models here.
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario','idade' ,'cpf', 'cidade', 'estado')
+    list_display_links = ('id', 'usuario')
+
+
+admin.site.register(Perfil, PerfilAdmin)
+
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from .enderecos import estados
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField()
+    cpf = models.CharField(unique=True, max_length=11)
+    enderecos = models.CharField(max_length=50)
+    complemento = models.CharField(max_length=5)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=estados,
+        )
+    
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+
+    class Meta:
+        verbose_name_plural = 'Perfis'
