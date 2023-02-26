309a4dae9fbb0c345e708e95fa9f9da2d07965cb
Derbson <pedrin.olinda@gmail.com>
2023-02-26 17:04:06 -0300

    criação os models Peido e ItensPedido

--- produto/admin.py
+++ produto/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from .models import Produto, Variacao
 
 
+class VariacaoInline(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
 class ProdutosAdmin(admin.ModelAdmin):
     list_display = ("id", "nome", "slug", "preco_marketing", "preco_promocional", "tipo")
     list_display_links = ("id", "nome")
+    inlines = [
+        VariacaoInline
+    ]
+    
 
 admin.site.register(Produto, ProdutosAdmin)
 admin.site.register(Variacao)
--- produto/models.py
+++ produto/models.py
@@ -64,3 +64,7 @@ class Variacao(models.Model):
     def __str__(self):
         return self.nome or self.produto.nome
     
+    class Meta:
+        verbose_name_plural = 'Variações'
+    
+    
